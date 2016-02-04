a8fbd17424fc68ece4706072c358c41fa88b5cea
sandeep <smukk9#gmail.com>
2016-02-04 11:41:47 -0800

    add Point

--- SpringDemo/src/org/sandeep/SpringDemo/Triangle.java
+++ SpringDemo/src/org/sandeep/SpringDemo/Triangle.java
@@ -2,18 +2,34 @@ package org.sandeep.SpringDemo;
 
 public class Triangle {
 
-	private String type;
+private Point pointA;
+private Point pointB;
+private Point pointC;
+public Point getPointC() {
+	return pointC;
+}
+public void setPointC(Point pointC) {
+	this.pointC = pointC;
+}
+public Point getPointA() {
+	return pointA;
+}
+public void setPointA(Point pointA) {
+	this.pointA = pointA;
+}
+public Point getPointB() {
+	return pointB;
+}
+public void setPointB(Point pointB) {
+	this.pointB = pointB;
+}
+
+public void draw(){
 	
-	public String getType() {
-		return type;
-	}
+	System.out.println("pointA:"+getPointA().getX() +"-" + getPointA().getY());
+	System.out.println("pointB:"+getPointB().getX() +"-" + getPointB().getY());
+	System.out.println("pointC:"+getPointC().getX() +"-" + getPointC().getY());
+}
 
-	public void setType(String type) {
-		this.type = type;
-	}
 
-	public void draw(){
-		
-		System.out.println(getType()+ "drawing Traingle");
-	}
 }
