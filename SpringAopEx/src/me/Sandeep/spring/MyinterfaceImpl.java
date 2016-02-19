ff0ce855ab5a45b26d651bb120aa88177d9ce009
sandeep <smukk9#gmail.com>
2016-02-18 23:30:44 -0800

    all clear

--- SpringAopEx/src/me/Sandeep/spring/MyinterfaceImpl.java
+++ SpringAopEx/src/me/Sandeep/spring/MyinterfaceImpl.java
@@ -4,12 +4,12 @@ public class MyinterfaceImpl implements Myinterafce{
 
 	@Override
 	public void m1() {
-		System.out.println("Hi i am form M1 method.")		
+		System.out.println("Hi i am form M1 method.")		;
 	}
 
 	@Override
 	public void m2() {
-		System.out.println("Hi iam from M2 method");;		
+		System.out.println("Hi iam from M2 method");		
 	}
 
 }
