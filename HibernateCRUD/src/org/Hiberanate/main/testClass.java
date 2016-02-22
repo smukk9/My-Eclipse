0bfa9267ba31f50e561cc98234e20bc7b716ff79
sandeep <smukk9#gmail.com>
2016-02-21 22:48:05 -0800

    querry features added

--- HibernateCRUD/src/org/Hiberanate/main/testClass.java
+++ HibernateCRUD/src/org/Hiberanate/main/testClass.java
@@ -1,6 +1,9 @@
 package org.Hiberanate.main;
 
+import java.util.List;
+
 import org.Hiberanate.DTO.userDetails;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -18,19 +21,30 @@ public class testClass {
 		Session s = sfc.openSession();
 		s.beginTransaction();
 		
-		userDetails user = (userDetails) s.get(userDetails.class, 5);
-		//System.out.println("User Name is :" + user.getUser_Details());
-		user.setUser_Details("newuser1");
-		s.update(user);
-		//s.delete(user);
+//		userDetails user = (userDetails) s.get(userDetails.class, 5);
+//		System.out.println("User Name is :" + user.getUser_Details());
+//		user.setUser_Details("newuser1");
+//		s.update(user);
+//		//s.delete(user);
 //		for(int i =0;i<10;i++){
 //			userDetails user = new userDetails();
 //			user.setUser_Details("user"+i);
 //		to CREATE
-	//	s.save(user);
-//		}
+//		s.save(user);
 		
+		//querry will give us the flexibility of using HQL queryies.
+		//pagination = imit the number of row you want to be in you search.
+		Query  querry = s.createQuery("from userDetails");
+		@SuppressWarnings({ "unchecked" })
 		
+		List<userDetails> user_List = (List<userDetails>)querry.list();
+		System.out.println("userDetails:"+user_List.size());
+		
+		for(userDetails u: user_List){
+			
+			System.out.println(u.getUser_Details());
+			System.out.println(u.getUser_Id()); 	
+		}
 		s.getTransaction().commit();
 		s.close();
 		
