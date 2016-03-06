a81ae6ddfceb487e0c961c4ceb9117a10514874b
sandeep <smukk9#gmail.com>
2016-03-06 12:46:39 -0800

    update servlet

--- firstServelet/src/org/fristserver/me/firstserver.java
+++ firstServelet/src/org/fristserver/me/firstserver.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class firstserver
@@ -18,25 +19,26 @@ public class firstserver extends HttpServlet {
        
   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-
-	//System.out.println("Hi there you are in DOGET now.");
-	//returns a printwriter object.
+	
 	PrintWriter writer = response.getWriter();
 	String username = request.getParameter("username");
-	
-	response.getWriter().println("this is from get method:"+username);
-	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	PrintWriter writer = response.getWriter();
-	String firstName = request.getParameter("firstName");
-	String lastName = request.getParameter("lastName");
-	String profession = request.getParameter("proffesion");
-	System.out.println(firstName+ lastName + profession);
-			
-	writer.println("Your fullname"+ firstName+lastName +"your prog:" +profession);
+	//response.getWriter().println("this is from get method:"+username +"\n");
+	HttpSession httpses = request.getSession();
+	httpses.setAttribute("saveduser", username);
+	writer.println("this is from the user:"+username +"\n");
+	writer.println("this is from the session:"+ (String)httpses.getAttribute("saveduser"));
 	
 	}
+	
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		
+//	PrintWriter writer = response.getWriter();
+//	String firstName = request.getParameter("firstName");
+//	String lastName = request.getParameter("lastName");
+//	String profession = request.getParameter("proffesion");
+//	System.out.println(firstName+ lastName + profession);
+//			
+//	writer.println("Your fullname"+ firstName+lastName +"your prog:" +profession);
+//	
+//	}
 }
